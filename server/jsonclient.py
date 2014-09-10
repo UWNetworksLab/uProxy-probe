b3fdcf94017e50d2f3a3f039951a0f786c1d0730
quantsword <quantsword@gmail.com>
2014-09-10 07:04:02 -0700

    update code, adding comment, remove unnessary stuff.

--- server/jsonclient.py
+++ server/jsonclient.py
@@ -13,12 +13,11 @@ import optparse
 import fileinput
 import socket
 
-ECHO_PORT = 50000 + 7
 BUFSIZE = 2048
 
 quit_prog = False
 
-def my_handler(signum, frame):
+def sigint_handler(signum, frame):
   global quit_prog
   print "keyboard event received, quitting"
   quit_prog = True
@@ -28,7 +27,7 @@ config = {}
 
 def main():
   global config
-  signal.signal(signal.SIGINT, my_handler)
+  signal.signal(signal.SIGINT, sigint_handler)
 
   parser = optparse.OptionParser()
   parser.add_option("-c", "--config", dest="config_filename",
@@ -36,8 +35,6 @@ def main():
                     help="configuration FILE in json format.", metavar="FILE")
   parser.add_option("--remote_host", dest="remote_host", 
                     help="HOST name for client to connect to", metavar="HOST")
-  parser.add_option("--local_port", dest="local_port", type="int",
-                    help="local PORT for this program to listen to", metavar="PORT")
   parser.add_option("--remote_port", dest="remote_port", type="int",
                     help="remote PORT for this client to connect to.", metavar="PORT")
 
@@ -46,27 +43,14 @@ def main():
   config = json.load(open(options.config_filename))
 
   if options.remote_port is not None:
-    config['remote_port'] = options.remote_port
-  if options.local_port is not None:
-    config['local_port'] = options.local_port
-  
+    config['remote_port'] = options.remote_port  
   if options.remote_host is not None:
     config['remote_host'] = options.host
 
-  if 'packet_len' not in config:
-    config['packet_len'] = 1111
-
-  client = UdpClient(config)
+  client = NatDetectClient(config)
   client.perform_job()
-  # time.sleep(5)
   client.quit_thread()
 
-
-def generate_packet(length):
-  data = bytearray(length)
-  for i in range(length):
-    data[i] = random.randint(0, 255)
-  return data
 
 class ReceivingThread(threading.Thread):
   def __init__(self, sock, rsp_queue):
@@ -80,30 +64,30 @@ class ReceivingThread(threading.Thread):
       ready = select.select([self.socket], [], [], 3)
       if ready[0]:
         rsp, fromaddr = self.socket.recvfrom(BUFSIZE)
-        # print 'response received from = %s:%s' % fromaddr
-        # print 'response = '
-        # print rsp
         self.rsp_queue.append(json.loads(rsp))
 
 
-class UdpClient:
+class NatDetectClient:
+  '''The client side implementation of NAT detection, running in user env.'''
+
   def __init__(self, config):
     self.config = config
 
   def perform_job(self):
     self.addr = config['remote_host'], config['remote_port']
     self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    # self.sock.bind(('', config['local_port']))
     self.sock.bind(('', 0))
     self.sock.setblocking(0)
     self.response_queue = []
 
+    # set a thread to receive udp packet and push them to a queue.
     self.recv_thread = ReceivingThread(self.sock, self.response_queue)
     self.recv_thread.start()
 
     if self.config['nat_type_query']:
       self.do_nat_type_query()
     else:
+      # this is really for debugging purpose. You can push anything to server.
       for line in fileinput.input():
         req += line;
       print 'request sent to %s:%s' % addr
@@ -175,8 +159,6 @@ class UdpClient:
           peer_addr = rsp['prepare_peer'].split(':')
           reflexive_addr1 = rsp['reflexive_addr']
           self.sock.sendto(json.dumps(req), (peer_addr[0], int(peer_addr[1])))
-          
-
         elif rsp['answer'] == 'SymmetricNAT':
           reflexive_addr2 = rsp['reflexive_addr']
           print '!!! Your NAT is symmetric type, 2 reflexive addresses seen'
--- server/jsonserv.py
+++ server/jsonserv.py
@@ -19,7 +19,7 @@ BUFSIZE = 2048
 
 quit_prog = False
 
-def my_handler(signum, frame):
+def sigint_handler(signum, frame):
     global quit_prog
     print "keyboard event received, quitting"
     quit_prog = True
@@ -28,7 +28,7 @@ def my_handler(signum, frame):
 config = {}
 
 def main():
-    signal.signal(signal.SIGINT, my_handler)
+    signal.signal(signal.SIGINT, sigint_handler)
 
     parser = optparse.OptionParser()
     parser.add_option("-c", "--config", dest="config_filename",
@@ -37,12 +37,8 @@ def main():
     parser.add_option("-v", "--verbose", dest="verbose", action="store_false",
                       default=True, 
                       help="if the session is verbose.")
-    parser.add_option("--host", dest="host", 
-                      help="HOST name for client to connect to", metavar="HOST")
     parser.add_option("--local_port", dest="local_port", type="int",
                       help="local PORT for this program to listen to", metavar="PORT")
-    parser.add_option("--remote_port", dest="remote_port", type="int",
-                      help="remote PORT for this client to connect to.", metavar="PORT")
 
     (options, args) = parser.parse_args()
     global config
@@ -52,8 +48,6 @@ def main():
         config['local_port'] = options.local_port
     if options.verbose is not None:
         config['verbose'] = options.verbose
-    elif config['mode'] is None:
-        config['mode'] = 'client'
     
     start_json_server(config)
     while not quit_prog:
@@ -70,7 +64,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
     """
     def handle(self):
         data = self.request[0].strip()
-        sock0 = self.request[1]
+        sock = self.request[1]
 
         if config['verbose']:
           print 'from=%s:%s' % self.client_address
@@ -84,7 +78,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             'answer': 'Pong',
             'ping_time': req['ping_time']
           }
-          sock0.sendto(json.dumps(rsp), self.client_address) 
+          sock.sendto(json.dumps(rsp), self.client_address) 
 
         # For full-cone detection, answer is sent from another ip/port.
         if req['ask'] == 'AmIFullCone':
@@ -92,15 +86,16 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             'answer': 'FullCone',
             'reflexive_addr': '%s:%s' % self.client_address
           }
-          sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-          sock1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-          sock1.bind((config['interface1'], config['local_port'] + 1003))
-          sock1.sendto(json.dumps(rsp), self.client_address)
+          random_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+          random_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+          # pick a random port
+          random_sock.bind((config['interface1'], 0))
+          random_sock.sendto(json.dumps(rsp), self.client_address)
 
         # For restricted-cone detection, server tell the client to prepare
         # the mapping to the ip that will be used to send the answer.
-        # Client can choose to adjust the port to differentiate between
-        # restricted cone and port restricted cone.
+        # Server instructs client to send to port 80, which is not being
+        # listened by server for sure.
         elif req['ask'] == 'AmIRestrictedCone':
           rsp0 = {
             'answer': 'RestrictedConePrepare',
@@ -108,7 +103,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             'prepare_peer': '%s:80' % config['extern_ip1']
           }
           for i in range(5):
-            sock0.sendto(json.dumps(rsp0), self.client_address)
+            sock.sendto(json.dumps(rsp0), self.client_address)
             time.sleep(0.3)
 
           rsp = {
@@ -119,10 +114,9 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             secondSocket.sendto(json.dumps(rsp), self.client_address)
             time.sleep(0.3)
 
-        # For restricted-cone detection, server tell the client to prepare
-        # the mapping to the ip/port that will be used to send the answer.
-        # Client can choose to adjust the port to differentiate between
-        # restricted cone and port restricted cone.
+        # For port restricted-cone detection, server tell the client to prepare
+        # the mapping to the ip/port that will be used to send the answer. This
+        # is the ip/port from which server will send the reply.
         elif req['ask'] == 'AmIPortRestrictedCone':
           second_port = config['second_port']
           rsp0 = {
@@ -131,7 +125,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             'prepare_peer': '%s:%s' % (config['extern_ip1'], second_port)
           }
           for i in range(5):
-            sock0.sendto(json.dumps(rsp0), self.client_address)
+            sock.sendto(json.dumps(rsp0), self.client_address)
             time.sleep(0.3)
 
           rsp = {
@@ -142,20 +136,22 @@ class UDPHandler(SocketServer.BaseRequestHandler):
             secondSocket.sendto(json.dumps(rsp), self.client_address)
             time.sleep(0.3)
 
-        # For symmetric NAT detection, we will need a second listenning server.
+        # For symmetric NAT detection, the client is asked to send 2nd request
+        # to 2nd ip/port pair. The reply need to be send in the same socket
+        # where request is received. 
         elif req['ask'] == 'AmISymmetricNAT':
           rsp0 = {
             'answer': 'SymmetricNATPrepare',
             'reflexive_addr': '%s:%s' % self.client_address,
             'prepare_peer': '%s:%s' %  (config['extern_ip1'], config['second_port'])
           }
-          sock0.sendto(json.dumps(rsp0), self.client_address)
+          sock.sendto(json.dumps(rsp0), self.client_address)
 
 
 class SymmetricUDPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         data = self.request[0].strip()
-        sock0 = self.request[1]
+        sock = self.request[1]
 
         if config['verbose']:
           print 'from=%s:%s' % self.client_address
@@ -169,7 +165,7 @@ class SymmetricUDPHandler(SocketServer.BaseRequestHandler):
             'reflexive_addr': '%s:%s' % self.client_address
           }
           for i in range(5):
-            sock0.sendto(json.dumps(rsp), self.client_address)
+            sock.sendto(json.dumps(rsp), self.client_address)
             time.sleep(0.3)
 
 
@@ -192,9 +188,13 @@ def start_json_server(config):
     server_thread.start()
     print "Server loop running in thread:", server_thread.name
 
-    server1 = ThreadedUDPServer((config['interface1'], config['local_port'] + 1000), SymmetricUDPHandler)
+    server1 = ThreadedUDPServer((config['interface1'], config['second_port']), SymmetricUDPHandler)
+
+    # record this second listen socket so that server can send reply from
+    # this ip/port, but not as response to request received by this ip/port.
     global secondSocket
     secondSocket = server1.socket
+
     # Start a thread with the server -- that thread will then start one
     # more thread for each request
     server_thread1 = threading.Thread(target=server1.serve_forever)
